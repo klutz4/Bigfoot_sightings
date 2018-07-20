1caf4f182d0434e4c6fc20cdbbe60d9d6e1dc565
Robert Dowd <robertdowd@Roberts-MacBook-Pro-3.local>
2018-07-20 13:19:43 -0600

    made get_content function

--- rob.py
+++ rob.py
@@ -7,23 +7,22 @@ import requests
 from io import StringIO
 
 
-import json
-
-reports = []
-with open('bigfoot_data.json') as f:
-    for i in f:
-        reports.append(json.loads(i))
-
-
-content_list = []
-data = pd.read_json('bigfoot_data.json', lines='true',orient='records')
-for i in range(1):
-    soup = BeautifulSoup(reports[i]['html'], 'html.parser')
+def get_content():
+    reports = []
+    with open('bigfoot_data.json') as f:
+        for i in f:
+            reports.append(json.loads(i))
 
 
+    content_list = []
+    data = pd.read_json('bigfoot_data.json', lines='true',orient='records')
+    for i in range(len(reports)):
+        soup = BeautifulSoup(reports[i]['html'], 'html.parser')
 
-    #soup = BeautifulSoup(data['html'])
+        tables = pd.read_html(data.iloc[i,1])
+        content = tables[3][0][0]
+        content_list.append(content)
 
-    tables = pd.read_html(data.iloc[i,1])
-    content = tables[3][0][0]
-    content_list.append(content)
+    return content_list
+
+content1 = get_content()
